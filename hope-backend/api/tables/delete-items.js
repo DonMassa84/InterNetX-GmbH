015882be9101ae2d9de5c9290e82de2ea2952e37
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-09-21 04:43:53 +0200

    Update delete-items.js

--- hope-backend/api/tables/delete-items.js
+++ hope-backend/api/tables/delete-items.js
@@ -1,33 +1,44 @@
-import connection from "../../db.js";
-import { selectOne } from "../query/select-one.js";
+const express = require('express');
+const router = express.Router();
+const db = require('../db.js');
+const bcrypt = require('bcrypt');
 
-export default function deleteItem(res, props) {
-  const { email, password } = props;
+router.delete('/user/:email', (req, res) => {
+    const email = req.params.email;
+    const auth = req.headers.authorization;
 
-  // Zuerst authentifizieren
-  connection.query(
-    selectOne("users", `WHERE email='${email}'`),
-    (err, rows) => {
-      if (err || rows.length === 0) {
-        return res.status(401).json("User not found or invalid credentials");
-      }
-
-      const user = rows[0];
-      if (!user.admin) {
-        return res.status(403).json("Not authorized as admin");
-      }
-
-      // Benutzer löschen
-      connection.query(
-        `DELETE FROM users WHERE email='${email}'`,
-        (err) => {
-          if (err) {
-            return res.status(500).json("Failed to delete user");
-          }
-
-          res.status(200).json({ message: "User successfully deleted" });
-        }
-      );
+    if (!auth) {
+        return res.status(401).json({ message: 'Authentifizierung erforderlich' });
     }
-  );
-}
+
+    const [username, password] = Buffer.from(auth.split(' ')[1], 'base64').toString().split(':');
+
+    const query = 'SELECT * FROM users WHERE email = ?';
+    db.query(query, [email], async (err, results) => {
+        if (err || results.length === 0) {
+            return res.status(500).json({ error: 'Benutzer nicht gefunden' });
+        }
+
+        const user = results[0];
+        const validPassword = await bcrypt.compare(password, user.password_hash);
+
+        if (username !== email || !validPassword) {
+            return res.status(401).json({ message: 'Ungültige Authentifizierungsdaten' });
+        }
+
+        if (!user.admin) {
+            return res.status(403).json({ message: 'Nur Admins können Benutzer löschen' });
+        }
+
+        const deleteQuery = 'DELETE FROM users WHERE email = ?';
+        db.query(deleteQuery, [email], (err) => {
+            if (err) {
+                return res.status(500).json({ error: 'Fehler beim Löschen des Benutzers' });
+            }
+            res.json({ message: 'Benutzer erfolgreich gelöscht' });
+        });
+    });
+});
+
+module.exports = router;
+
