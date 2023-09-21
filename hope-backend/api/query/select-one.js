f23ef7cfc5ec88a9464dcded21d2eca2e1f5a2eb
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-09-21 04:42:14 +0200

    Update select-one.js

--- hope-backend/api/query/select-one.js
+++ hope-backend/api/query/select-one.js
@@ -15,6 +15,39 @@
 *     }
 *
 */
-export const selectOne = (table, condition) => {
-  return `SELECT * FROM ${table} ${condition}`;
-};
+const express = require('express');
+const router = express.Router();
+const db = require('../db.js');
+const bcrypt = require('bcrypt');
+
+router.get('/user/:email', (req, res) => {
+    const email = req.params.email;
+    const auth = req.headers.authorization;
+
+    if (!auth) {
+        return res.status(401).json({ message: 'Authentifizierung erforderlich' });
+    }
+
+    const [username, password] = Buffer.from(auth.split(' ')[1], 'base64').toString().split(':');
+
+    const query = 'SELECT * FROM users WHERE email = ?';
+    db.query(query, [email], async (err, results) => {
+        if (err || results.length === 0) {
+            return res.status(500).json({ error: 'Benutzer nicht gefunden' });
+        }
+
+        const user = results[0];
+        const validPassword = await bcrypt.compare(password, user.password_hash);
+
+        if (username !== email || !validPassword) {
+            return res.status(401).json({ message: 'Ungültige Authentifizierungsdaten' });
+        }
+
+        delete user.password_hash;
+        delete user.salt;
+        res.json(user);
+    });
+});
+
+module.exports = router;
+
