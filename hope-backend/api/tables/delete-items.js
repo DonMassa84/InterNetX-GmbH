b4f83981f35096d513a80696be2ba1cc910c1122
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-09-21 04:20:17 +0200

    Update delete-items.js

--- hope-backend/api/tables/delete-items.js
+++ hope-backend/api/tables/delete-items.js
@@ -1,11 +1,33 @@
 import connection from "../../db.js";
-import {deleteOne} from "../query/delete-one.js";
-import {deleteCallback } from "..//../utils/request-callback.js";
+import { selectOne } from "../query/select-one.js";
 
-export default async function deleItem(res, props) {
-  const { dbTableName, id } = props;
+export default function deleteItem(res, props) {
+  const { email, password } = props;
 
-await connection.query('${deleteOne(dbTableName, id)}', (err, row) =>
-  requestCallback(res, row, err)
-                       );
+  // Zuerst authentifizieren
+  connection.query(
+    selectOne("users", `WHERE email='${email}'`),
+    (err, rows) => {
+      if (err || rows.length === 0) {
+        return res.status(401).json("User not found or invalid credentials");
+      }
+
+      const user = rows[0];
+      if (!user.admin) {
+        return res.status(403).json("Not authorized as admin");
+      }
+
+      // Benutzer löschen
+      connection.query(
+        `DELETE FROM users WHERE email='${email}'`,
+        (err) => {
+          if (err) {
+            return res.status(500).json("Failed to delete user");
+          }
+
+          res.status(200).json({ message: "User successfully deleted" });
+        }
+      );
+    }
+  );
 }
