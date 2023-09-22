d9de20463d8e259c3fc4b65bbed5f80bd8b2b303
Maximilian Trustworth <100371773+DonMassa84@users.noreply.github.com>
2023-09-22 02:31:53 +0200

    Update delete-items.js

--- hope-backend/api/tables/delete-items.js
+++ hope-backend/api/tables/delete-items.js
@@ -6,30 +6,23 @@ const bcrypt = require('bcrypt');
 router.delete('/user/:email', (req, res) => {
     const email = req.params.email;
     const auth = req.headers.authorization;
-
     if (!auth) {
         return res.status(401).json({ message: 'Authentifizierung erforderlich' });
     }
-
     const [username, password] = Buffer.from(auth.split(' ')[1], 'base64').toString().split(':');
-
     const query = 'SELECT * FROM users WHERE email = ?';
     db.query(query, [email], async (err, results) => {
         if (err || results.length === 0) {
             return res.status(500).json({ error: 'Benutzer nicht gefunden' });
         }
-
         const user = results[0];
         const validPassword = await bcrypt.compare(password, user.password_hash);
-
         if (username !== email || !validPassword) {
             return res.status(401).json({ message: 'Ungültige Authentifizierungsdaten' });
         }
-
         if (!user.admin) {
             return res.status(403).json({ message: 'Nur Admins können Benutzer löschen' });
         }
-
         const deleteQuery = 'DELETE FROM users WHERE email = ?';
         db.query(deleteQuery, [email], (err) => {
             if (err) {
@@ -41,4 +34,3 @@ router.delete('/user/:email', (req, res) => {
 });
 
 module.exports = router;
-
